788d79403b4a5d0268cd9734f0bb9b81a863facb
Shubham Jadhav <shubhamjadhv3182@gmail.com>
2023-07-24 16:18:28 +0530

    video16

--- src/App.js
+++ src/App.js
@@ -1,54 +1,54 @@
-import './App.css';
-import Alert from './components/Alert';
+import "./App.css";
 // import About from './components/About';
-import Navbar from './components/Navbar';
-import TextForm from './components/TextForm';
-import React, { useState } from 'react';
+import Alert from "./components/Alert";
+import Navbar from "./components/Navbar";
+import TextForm from "./components/TextForm";
+import React, { useState } from "react";
 
 function App() {
-  const [mode, setMode] = useState('light'); //Whether dark mode is enabled or not
-  const [alert, setAlert]=useState(null);
+  const [mode, setMode] = useState("light"); //Whether dark mode is enabled or not
+  const [alert, setAlert] = useState(null);
 
-  const showAlert = (message, type)=>{
-      setAlert({
-        msg: message,
-        type: type
-      })
-      setTimeout(() =>{
-        setAlert(null);
-      },1500);
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type,
+    });
+    setTimeout(() => {
+      setAlert(null);
+    }, 1500);
+  };
 
-  }
-
-  const toggleMode = ()=>{
-  if(mode === 'light'){
-    setMode('dark');
-    document.body.style.background = '#230653';
-    document.body.style.color = '#fff';
-    showAlert("Dark mode has been enabled","success");
-    document.title = 'TextUtils - Dark Mode';
-  }
-  else{
-    setMode('light');
-    document.body.style.background = 'white';
-    document.body.style.color = 'black';
-    showAlert("light mode has been enabled","success");
-    document.title = 'TextUtils - light Mode';
-    
-
-  }
-}
+  const toggleMode = () => {
+    if (mode === "light") {
+      setMode("dark");
+      document.body.style.background = "#230653";
+      document.body.style.color = "#fff";
+      showAlert("Dark mode has been enabled", "success");
+      document.title = "TextUtils - Dark Mode";
+    } else {
+      setMode("light");
+      document.body.style.background = "white";
+      document.body.style.color = "black";
+      showAlert("light mode has been enabled", "success");
+      document.title = "TextUtils - light Mode";
+    }
+  };
   return (
     <>
-{/* <Navbar title="TextUtils" aboutText="About TextUtils"/> */}
-{/* <Navbar/> */}
-<Navbar title="TextUtils" mode={mode}toggleMode={toggleMode}/>
-<Alert alert={alert}/>
-<div className="container my-3">
-<TextForm showAlert={showAlert} heading="Enter The text to analyze below" mode={mode}/>
- {/* <About/> */}
-  </div>
-</>
+      {/* <Navbar title="TextUtils" aboutText="About TextUtils"/> */}
+      {/* <Navbar/> */}
+      <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} />
+      <Alert alert={alert} />
+      <div className="container my-3">
+        <TextForm
+          showAlert={showAlert}
+          heading="Enter The text to analyze below"
+          mode={mode}
+        />
+        {/* <About/> */}
+      </div>
+    </>
   );
 }
 
